e28bd1c79a9fd1fa6e53c6028ba7063580c86fc1
soichiro-nitta <soic1226@gmail.com>
2021-01-23 16:10:08 +0900

    add inset

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,8 @@ export const tailwindConfig = {
     '-14': '-3.5rem',
     '-11': '-2.75rem',
     '-8': '-2rem',
+    '-px': '-1px',
+    px: '1px',
     1: '0.25rem',
     2: '0.5rem',
     4: '1rem',
